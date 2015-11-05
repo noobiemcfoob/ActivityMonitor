94ba354f58a1571545f94e3bb3993e9e1495f76a
noobiemcfoob <noobiemcfoob@gmail.com>
2015-11-04 21:10:41 -0500

    Functional ActivityLogReader with Dataset Builder
    Log reader is now functional with dataset averager over a given time
    window for given time intervals. Next step: Formalize graphs and allow
    report generation

--- ActivityLogReader.py
+++ ActivityLogReader.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import csv
 import math
 from embeddedShell import ipshell
+from sys import stdout
 
 activity_readings = list()
 
@@ -30,16 +31,82 @@ class ActivityDataPoint(object):
     self.activity     = activity
     self.active_app   = active_app
 
+  def __repr__(self):
+    return "|| Activity {0:s} | {1:s} ||".format( str(self.timestamp), str(self.activity) )
+
+class IntervalDataPoint(ActivityDataPoint):
+  def __init__( 
+    self,
+    timestamp
+  ):
+    self.timestamp            = timestamp
+    self.activity             = 0
+    self.total_activity       = 0
+    self.total_observations   = 0
+
+  def AddObservation(self, observation):
+    if not type(observation) is ActivityDataPoint: raise Exception("Expecting AcitivtyDataPoint for observation")
+
+    self.total_activity += observation.activity
+    self.total_observations += 1
+
+    self.activity = self.total_activity / self.total_observations
+
 def TimeFloor(time, minutes):
-  return time - datetime.timedelta(minutes=time.minutes%minutes, seconds=now.second)
+  return time - datetime.timedelta(minutes=time.minute%minutes, seconds=time.second)
+
+def DisplayBinarySearchProgress(out_string):
+  if out_string != DisplayBinarySearchProgress.out_string:
+    print(out_string)
+  else:
+    stdout.write("\rReproduced: "+out_string)
+    stdout.flush()
+
+DisplayBinarySearchProgress.out_string = ""
+
+def BinarySearch(test_value, data, imin=0, imax=None, debug=False):
+  if imax is None:
+    imax = len(data)-1
+
+  index = imin + int( (imax - imin)/2 )
+
+  output_string = ""
+  if debug: 
+    output_string = "\n\n(TV: %s) Index: %d - %s ||\nIMIN: %d - %s || IMAX: %d - %s"%(
+                      test_value.strftime("%c"),
+                      index,
+                      data[index].strftime("%c"),
+                      imin,
+                      data[imin].strftime("%c"),
+                      imax,
+                      data[imax].strftime("%c")
+                    )
+
+  if test_value < data[index]:
+    if debug: output_string += "\nValue lower than index. Moving upper end"
+    DisplayBinarySearchProgress(output_string)
+    if index == imax:
+      return index
+    index = BinarySearch(test_value, data, imin=imin, imax=index, debug=debug)
+  elif test_value > data[index]:
+    if debug: output_string += "\nValue greater than index. Moving lower end"
+    DisplayBinarySearchProgress(output_string)
+    if index == imin:
+      return imin 
+    index = BinarySearch(test_value, data, imin=index, imax=imax, debug=debug)
+  else:
+    if debug: output_string += "\nMatched value at index %d"%(index)
+    DisplayBinarySearchProgress(output_string)
+    
+  return index
 
 ##
-# Read a log and fill unobserved times with blank data
+# Read a log, fill unobserved times with blank data, and average data into predefined intervals
 # start_time    = Starting time to begin filling in data set
 # time_interval = number of minutes for time window 
-def BuildDataSet(start_time=None, end_time=None, time_interval=10, activities=None):
+def BuildAveragedDataSet(start_time=None, end_time=None, time_interval=10, activities=None, debug=False):
   dataset = list()
-  interval_delta = datetime.delta(minutes=time_interval)
+  interval_delta = datetime.timedelta(minutes=time_interval)
   if start_time is None:
     start_time = TimeFloor(activities[0].timestamp, time_interval)
 
@@ -49,25 +116,48 @@ def BuildDataSet(start_time=None, end_time=None, time_interval=10, activities=No
   if start_time > end_time:
     raise Exception("BuildDataSet || Start time is after End Time")
 
-  num_intervals = math.ceil( (end_time-start_time).total_seconds()/60 )
+  num_intervals = math.ceil( (end_time-start_time).total_seconds()/(60*time_interval) )
+  # Build a num_intervals long list containing ActivityDataPoint at correct timestamp (empty)
   interval_time = start_time
-  current_activities = list()
-  for activity in activities:
-    activity_timefloor = TimeFloor(activity.timestamp, time_interval) 
-    # Pad dataset with empty interval windows
-    while activity_timefloor > interval_time:
-      dataset.append( ActivityDataPoint( interval_time, False, None ), () )
-      interval_time += interval_delta
+  for interval in range(0,num_intervals):
+    dataset.append( IntervalDataPoint(interval_time) )
+    interval_time += interval_delta
 
-    if activity_timefloor != interval_time:
-      raise Exception("Something went really wrong...")
+  print("BuildDataSet || Number of bins defined: ", len(dataset), "from", dataset[0].timestamp, "to", dataset[-1].timestamp)
 
-    #Check data set time. If Time is not in window (ahead), fill with zeros
+  # Build a list of just the timestamps
+  interval_timestamps = [data.timestamp for data in dataset]
+
+  def DisplayObservationNumber(number):
+    stdout.write("\rOn Observation %d of %d"%(number, len(activities)-1))
+    stdout.flush()
+
+  # Cycle through all activities and sort into interval bins
+  for index,activity in enumerate(activities):
+    DisplayObservationNumber(index)
+    # Floor the timestamp of the acivity to within the time_interval
+    activity_timefloor = TimeFloor(activity.timestamp, time_interval)
+    # Check that activity is in desired time window
+    if activity_timefloor < start_time or activity_timefloor > end_time:
+      continue
+
+    # Determine which bin to put the observation
+    # print("Searching for time bin for {0:s}".format( str(activity) ))
+    activity_bin_index = BinarySearch(activity_timefloor, interval_timestamps, debug=True)
+
+    try:
+      print( "\nFound bin for {0:s} at index {1:d}".format( str(activity), activity_bin_index ) )
+      print( "Time Bin: {0:s}".format( str(dataset[activity_bin_index]) ) )
+      dataset[activity_bin_index].AddObservation( activity )
+    except Exception as e:
+      print("\n\nFailed to add {0:s} at index: {1:d}".format( str(activity), activity_bin_index ))
+      raise e
+
+  return dataset
 
 
 if __name__ == '__main__':
   print("Running ReadLog")
   ReadLog()
   ipshell()
-
 
